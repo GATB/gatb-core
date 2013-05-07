95bfbb28db6a45e6cfc27770c531c4bfa9b50af8
edrezen <edrezen@irisa.fr>
2013-05-07 18:37:09 +0200

    Add missing include

--- gatb-core/src/gatb/system/api/Exception.hpp
+++ gatb-core/src/gatb/system/api/Exception.hpp
@@ -28,6 +28,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <string>
 
 /********************************************************************************/
 namespace gatb      {
