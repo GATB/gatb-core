0e66fd142b37f8887442bae6ed61e84742cb8f27
edrezen <edrezen@irisa.fr>
2013-05-07 18:36:44 +0200

    first version

---
+++ gatb-core/src/gatb/tools/misc/api/Macros.hpp
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ *   GATB : Genome Assembly Tool Box                                         *
+ *   Copyright (c) 2013                                                      *
+ *                                                                           *
+ *   GATB is free software; you can redistribute it and/or modify it under   *
+ *   the CECILL version 2 License, that is compatible with the GNU General   *
+ *   Public License                                                          *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   CECILL version 2 License for more details.                              *
+ *****************************************************************************/
+
+/** \file Macros.hpp
+ *  \date 01/03/2013
+ *  \author edrezen
+ *  \brief Several macros for every day work
+ */
+
+#ifndef _GATB_CORE_TOOLS_MISC_MACROS_HPP_
+#define _GATB_CORE_TOOLS_MISC_MACROS_HPP_
+
+/********************************************************************************/
+namespace gatb      {
+namespace core      {
+namespace tools     {
+namespace misc      {
+/********************************************************************************/
+
+#define ARRAY_SIZE(a)  (sizeof(a)/sizeof(a[0]))
+
+/********************************************************************************/
+} } } } /* end of namespaces. */
+/********************************************************************************/
+
+#endif /* _GATB_CORE_TOOLS_MISC_MACROS_HPP_ */
