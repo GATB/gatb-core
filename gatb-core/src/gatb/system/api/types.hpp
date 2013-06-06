c8501da37532c08b65a58febf0854d96cbe1dba8
edrezen <edrezen@irisa.fr>
2013-06-06 14:56:28 +0200

    Modifying license headers

--- gatb-core/src/gatb/system/api/types.hpp
+++ gatb-core/src/gatb/system/api/types.hpp
@@ -1,15 +1,8 @@
 /*****************************************************************************
  *   GATB : Genome Assembly Tool Box                                         *
- *   Copyright (c) 2013                                                      *
- *                                                                           *
- *   GATB is free software; you can redistribute it and/or modify it under   *
- *   the CECILL version 2 License, that is compatible with the GNU General   *
- *   Public License                                                          *
- *                                                                           *
- *   This program is distributed in the hope that it will be useful,         *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
- *   CECILL version 2 License for more details.                              *
+ *   Authors: [R.Chikhi, G.Rizk, E.Drezen]                                   *
+ *   Based on Minia, Authors: [R.Chikhi, G.Rizk], CeCILL license             *
+ *   Copyright (c) INRIA, CeCILL license, 2013                               *
  *****************************************************************************/
 
 /** \file types.hpp
