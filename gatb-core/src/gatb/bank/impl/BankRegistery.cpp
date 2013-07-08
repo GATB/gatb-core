f99fe3fac63c0609d2fe3a34c9a23be0b1e306d9
edrezen <edrezen@irisa.fr>
2013-07-08 17:18:31 +0200

    First version

---
+++ gatb-core/src/gatb/bank/impl/BankRegistery.cpp
@@ -0,0 +1,99 @@
+/*****************************************************************************
+ *   GATB : Genome Assembly Tool Box                                         *
+ *   Authors: [R.Chikhi, G.Rizk, E.Drezen]                                   *
+ *   Based on Minia, Authors: [R.Chikhi, G.Rizk], CeCILL license             *
+ *   Copyright (c) INRIA, CeCILL license, 2013                               *
+ *****************************************************************************/
+
+#include <gatb/bank/impl/BankRegistery.hpp>
+
+#include <gatb/bank/impl/Bank.hpp>
+#include <gatb/bank/impl/BankBinary.hpp>
+
+#include <gatb/system/api/Exception.hpp>
+
+using namespace std;
+
+#define DEBUG(a)  //printf a
+
+/********************************************************************************/
+namespace gatb {  namespace core {  namespace bank {  namespace impl {
+/********************************************************************************/
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+void BankRegistery::registerFactory (const std::string& name, IBankFactory* instance)
+{
+    /** We look whether the factory is already registered. */
+    std::map <std::string, IBankFactory*>::iterator it = _factories.find (name);
+    if (it == _factories.end())
+    {
+        _factories[name] = instance;
+        instance->use();
+    }
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+IBankFactory* BankRegistery::getFactory (const std::string& name)
+{
+    IBankFactory* result = 0;
+
+    if (name.empty())  {  result = _factories[_defaultName]; }
+    else               {  result = _factories[name];         }
+
+    if (result == 0)  { throw system::Exception ("Bank factory not registered for name '%s'", name.c_str()); }
+
+    /** We return the factory. */
+    return result;
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+BankRegistery::BankRegistery ()
+{
+    /** We register most known factories. */
+    registerFactory ("fasta",  new BankFactory());
+    registerFactory ("binary", new BankBinaryFactory());
+
+    /** We set the default one. */
+    _defaultName = "fasta";
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+BankRegistery::~BankRegistery ()
+{
+    for (std::map <std::string, IBankFactory*>::iterator it = _factories.begin (); it != _factories.end(); it++)
+    {
+        (it->second)->forget ();
+    }
+}
+
+/********************************************************************************/
+} } } } /* end of namespaces. */
+/********************************************************************************/
---
+++ gatb-core/src/gatb/bank/impl/BankRegistery.hpp
@@ -0,0 +1,63 @@
+/*****************************************************************************
+ *   GATB : Genome Assembly Tool Box                                         *
+ *   Authors: [R.Chikhi, G.Rizk, E.Drezen]                                   *
+ *   Based on Minia, Authors: [R.Chikhi, G.Rizk], CeCILL license             *
+ *   Copyright (c) INRIA, CeCILL license, 2013                               *
+ *****************************************************************************/
+
+/** \file BankFactory.hpp
+ *  \date 01/03/2013
+ *  \author edrezen
+ *  \brief Interface definition for genomic databases management
+ */
+
+#ifndef _GATB_CORE_BANK_IMPL_BANK_FACTORY_HPP_
+#define _GATB_CORE_BANK_IMPL_BANK_FACTORY_HPP_
+
+/********************************************************************************/
+
+#include <gatb/bank/api/IBank.hpp>
+
+#include <string>
+#include <map>
+
+/********************************************************************************/
+namespace gatb      {
+namespace core      {
+namespace bank      {
+namespace impl      {
+/********************************************************************************/
+
+/** \brief TBD
+ */
+class BankRegistery
+{
+public:
+
+    /** Singleton instance. */
+    static BankRegistery& singleton()  { static BankRegistery instance; return instance; }
+
+    /** */
+    void registerFactory (const std::string& name, IBankFactory* instance);
+
+    /** */
+    IBankFactory* getFactory (const std::string& name = "");
+
+private:
+
+    /** Private due to singleton method. */
+    BankRegistery ();
+
+    /** */
+    ~BankRegistery ();
+
+    std::map <std::string, IBankFactory*> _factories;
+
+    std::string _defaultName;
+};
+
+/********************************************************************************/
+} } } } /* end of namespaces. */
+/********************************************************************************/
+
+#endif /* _GATB_CORE_BANK_IMPL_BANK_FACTORY_HPP_ */
