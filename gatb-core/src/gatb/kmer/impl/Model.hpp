79a097105c72814c69d0eebd5b32d28780e6a82d
edrezen <edrezen@irisa.fr>
2013-07-12 16:39:29 +0200

    Introduce a way to deal with BINARY data => remove the previously BINARY to INTEGER conversion. Note: could be improved (for perf)

--- gatb-core/src/gatb/kmer/impl/Model.hpp
+++ gatb-core/src/gatb/kmer/impl/Model.hpp
@@ -68,40 +68,22 @@ public:
     }
 
     /** */
-    void build (tools::misc::Data& d, std::vector<kmer_type>& kmersBuffer,KmerMode mode = KMER_MINIMUM)
+    void build (tools::misc::Data& data, std::vector<kmer_type>& kmersBuffer,KmerMode mode = KMER_MINIMUM)
     {
-        tools::misc::Data  binaryData;
-
-        /** By default, we will use the provided data with a ASCII encoding. */
-        tools::misc::Data* data = &d;
-
-        /** We may have to expand the binary data to integer format. */
-        if (d.getEncoding() == tools::misc::Data::BINARY)
-        {
-            size_t expandedLen = d.size() ;
-
-            if (expandedLen > binaryData.size())  {  binaryData.resize (expandedLen + 4);  }
-
-            /** We convert the provided binary data into integer encoding. */
-            tools::misc::Data::convert (d, binaryData);
-
-            data = &binaryData;
-        }
-
         /** We compute the number of kmers for the provided data. Note that we have to check that we have
          * enough nucleotides according to the current kmer size. */
-        int32_t nbKmers = data->size() - this->getSpan() + 1;
+        int32_t nbKmers = data.size() - this->getSpan() + 1;
         if (nbKmers <= 0)  { return; }
 
         /** We resize the resulting kmers vector. */
         kmersBuffer.resize (nbKmers);
 
         /** Shortcut used for computing kmers recursively. */
-        char* buffer = data->getBuffer() + this->getSpan() - 1;
+        char* buffer = data.getBuffer() + this->getSpan() - 1;
 
         /** We compute the first kmer as a polynomial value. */
-        kmer_type graine  = ModelAbstract<kmer_type>::codeSeed (data->getBuffer(), data->getEncoding(), KMER_DIRECT);
-        kmer_type revcomp = ModelAbstract<kmer_type>::codeSeed (data->getBuffer(), data->getEncoding(), KMER_REVCOMP);
+        kmer_type graine  = ModelAbstract<kmer_type>::codeSeed (data.getBuffer(), data.getEncoding(), KMER_DIRECT);
+        kmer_type revcomp = ModelAbstract<kmer_type>::codeSeed (data.getBuffer(), data.getEncoding(), KMER_REVCOMP);
         if(mode == KMER_MINIMUM)
         {
             kmersBuffer[0]    = std::min (graine, revcomp);
@@ -118,7 +100,7 @@ public:
          *  BUT, using function pointer would lead to many function calls with bad performance. We prefer therefore
          *  duplicate code and let inline work when needed.
          */
-        if (data->getEncoding() == tools::misc::Data::ASCII && mode == KMER_MINIMUM)
+        if (data.getEncoding() == tools::misc::Data::ASCII && mode == KMER_MINIMUM)
         {
             /** We compute the next kmers in a recursive way. */
             for (size_t i=1; i<nbKmers; i++)
@@ -129,7 +111,7 @@ public:
                 kmersBuffer[i] = std::min (graine, revcomp);
             }
         }
-        else if (data->getEncoding() == tools::misc::Data::INTEGER && mode == KMER_MINIMUM)
+        else if (data.getEncoding() == tools::misc::Data::INTEGER && mode == KMER_MINIMUM)
         {
             /** We compute the next kmers in a recursive way. */
             for (size_t i=1; i<nbKmers; i++)
@@ -140,7 +122,24 @@ public:
                 kmersBuffer[i] = std::min (graine, revcomp);
             }
         }
-        else if (data->getEncoding() == tools::misc::Data::ASCII && mode == KMER_DIRECT) //GR modif, check this with erwan
+        else if (data.getEncoding() == tools::misc::Data::BINARY && mode == KMER_MINIMUM)
+        {
+            buffer = data.getBuffer();
+
+            size_t i0 = this->getSpan();
+            size_t i1 = data.size();
+
+            /** We compute the next kmers in a recursive way. */
+            for (size_t i=i0; i<i1; i++)
+            {
+                char c = ((buffer[i>>2] >> ((3-(i&3))*2)) & 3);
+
+                graine  = ( (graine  << 2) +  c                     ) & this->_kmerMask;
+                revcomp = ( (revcomp >> 2) +  this->_revcompTable[c]) & this->_kmerMask;
+                kmersBuffer[i-i0+1] = std::min (graine, revcomp);
+            }
+        }
+        else if (data.getEncoding() == tools::misc::Data::ASCII && mode == KMER_DIRECT) //GR modif, check this with erwan
         {
             /** We compute the next kmers in a recursive way. */
             for (size_t i=1; i<nbKmers; i++)
@@ -150,12 +149,22 @@ public:
                 kmersBuffer[i] = graine;
             }
         }
-        else if (data->getEncoding() == tools::misc::Data::INTEGER && mode == KMER_DIRECT)
+        else if (data.getEncoding() == tools::misc::Data::INTEGER && mode == KMER_DIRECT)
         {
             /** We compute the next kmers in a recursive way. */
             for (size_t i=1; i<nbKmers; i++)
             {
                 char c = ModelAbstract<kmer_type>::NTidentity(buffer[i]);
+                graine  = ( (graine  << 2) +  c                     ) & this->_kmerMask;
+                kmersBuffer[i] = graine;
+            }
+        }
+        else if (data.getEncoding() == tools::misc::Data::BINARY && mode == KMER_DIRECT)
+        {
+            /** We compute the next kmers in a recursive way. */
+            for (size_t i=1; i<nbKmers; i++)
+            {
+                char c = ((buffer[i>>2] >> ((3-(i&3))*2)) & 3);
                 graine  = ( (graine  << 2) +  c                     ) & this->_kmerMask;
                 kmersBuffer[i] = graine;
             }
