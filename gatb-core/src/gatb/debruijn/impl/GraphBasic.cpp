2548133d6d5f0f6d42b80ab92a731ba0aba38325
edrezen <edrezen@irisa.fr>
2013-08-22 13:56:40 +0200

    Move code from hpp to cpp

--- gatb-core/src/gatb/debruijn/impl/GraphBasic.cpp
+++ gatb-core/src/gatb/debruijn/impl/GraphBasic.cpp
@@ -7,18 +7,23 @@
 
 #include <gatb/debruijn/impl/GraphBasic.hpp>
 
-#include <gatb/tools/math/NativeInt64.hpp>
-#include <gatb/tools/math/NativeInt128.hpp>
-#include <gatb/tools/math/LargeInt.hpp>
+#include <gatb/kmer/impl/DSKAlgorithm.hpp>
+#include <gatb/kmer/impl/DebloomAlgorithm.hpp>
+
+#include <gatb/tools/misc/impl/Property.hpp>
 
 using namespace std;
 using namespace gatb::core::tools::math;
+
+using namespace gatb::core::kmer::impl;
 
 #define DEBUG(a)  //printf a
 
 /********************************************************************************/
 namespace gatb {  namespace core {  namespace debruijn {  namespace impl {
 /********************************************************************************/
+
+static const double lg2 = 0.69314718055994530941723212145817656808;
 
 /*********************************************************************
 ** METHOD  :
@@ -28,6 +33,260 @@ namespace gatb {  namespace core {  namespace debruijn {  namespace impl {
 ** RETURN  :
 ** REMARKS :
 *********************************************************************/
+template<typename T>
+GraphBasic<T>::GraphBasic (tools::misc::IProperties* props)
+    : _nodesIterable(0), _bloom(0), _cFPset(0), _model(0), _props(0)
+{
+    setProps (props);
+
+    setNodesIterable (new tools::collections::impl::IterableFile<T> (props->getStr (STR_KMER_SOLID)));
+
+    setModel (new kmer::impl::Model<T> (props->getInt(STR_KMER_SIZE)));
+
+    /** We compute the bloom size. */
+    float NBITS_PER_KMER = log (16*props->getInt(STR_KMER_SIZE)*(lg2*lg2))/(lg2*lg2);
+    u_int64_t bloomSize =  (u_int64_t) (system::impl::System::file().getSize (props->getStr (STR_KMER_SOLID)) / sizeof (T) * NBITS_PER_KMER);
+
+    setBloom (kmer::impl::BloomBuilder<T> (_nodesIterable->iterator(), bloomSize, 7).build ());
+
+    if (props->get(STR_KMER_CFP) != 0)
+    {
+        setcFPset (new tools::collections::impl::ContainerSet<T> (
+            new tools::collections::impl::IteratorFile<T> (props->getStr(STR_KMER_CFP))
+        ));
+    }
+
+    _mask = _model->getMask ();
+    _span = _model->getSpan ();
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+template<typename T>
+GraphBasic<T>::GraphBasic (
+    tools::collections::Iterable<T>* solidKmers,
+    tools::collections::Iterable<T>* cFPKmers,
+    size_t kmerSize
+)
+    : _nodesIterable(0), _bloom(0), _cFPset(0), _model(0), _props(0)
+{
+    setNodesIterable (solidKmers);
+
+    setModel (new kmer::impl::Model<T> (kmerSize));
+
+    /** We compute the bloom size. */
+    double lg2 = log(2);
+    float NBITS_PER_KMER = log (16*kmerSize*(lg2*lg2))/(lg2*lg2);
+    u_int64_t bloomSize =  (u_int64_t) (solidKmers->getNbItems() * NBITS_PER_KMER);
+
+    setBloom (kmer::impl::BloomBuilder<T> (_nodesIterable->iterator(), bloomSize, 7).build ());
+
+    setcFPset (new tools::collections::impl::ContainerSet<T> (cFPKmers->iterator()));
+
+    _mask = _model->getMask ();
+    _span = _model->getSpan ();
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+template<typename T>
+GraphBasic<T>::GraphBasic (bank::IBank* bank, size_t kmerSize, size_t nks)
+    : _nodesIterable(0), _bloom(0), _cFPset(0), _model(0), _props(0)
+{
+    /** We set the model. */
+    setModel (new kmer::impl::Model<T> (kmerSize));
+    _mask = _model->getMask ();
+    _span = _model->getSpan ();
+
+    /** We create a DSK instance and execute it. */
+    DSKAlgorithm<T> dsk (bank, kmerSize, nks);
+    dsk.execute();
+
+    /** We create a debloom instance and execute it. */
+    DebloomAlgorithm<T> debloom (dsk.getSolidKmers(), kmerSize);
+    debloom.execute();
+
+    /** We set the solid and critical kmers iterables. */
+    setNodesIterable (dsk.getSolidKmers());
+    setcFPset        (new tools::collections::impl::ContainerSet<T> (debloom.getCriticalKmers()->iterator()));
+
+    /** We compute the bloom size. */
+    float NBITS_PER_KMER = log (16*kmerSize*(lg2*lg2))/(lg2*lg2);
+    u_int64_t bloomSize =  (u_int64_t) (dsk.getSolidKmers()->getNbItems() * NBITS_PER_KMER);
+
+    DEBUG (("nbSolid=%d  NBITS_PER_KMER=%f  bloomSize=%d \n", dsk.getSolidKmers()->getNbItems(), NBITS_PER_KMER, bloomSize));
+
+    /** We build the bloom filter. */
+    setBloom (kmer::impl::BloomBuilder<T> (dsk.getSolidKmers()->iterator(), bloomSize, 7).build ());
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+template<typename T>
+GraphBasic<T>::~GraphBasic ()
+{
+    setProps         (0);
+    setNodesIterable (0);
+    setcFPset        (0);
+    setModel         (0);
+    setBloom         (0);
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+template<typename T>
+size_t GraphBasic<T>::getEdges (const Node<T>& source, EdgeSet<T>& edges, Direction direction)
+{
+    size_t idx = 0;
+
+    // the kmer we're extending may be actually a revcomp sequence in the bidirected debruijn graph node
+    T graine = ((source.strand == STRAND_FORWARD) ?  source.kmer : revcomp (source.kmer, _span));
+
+    if (direction == DIR_OUTCOMING)
+    {
+        for (u_int64_t nt=0; nt<4; nt++)
+        {
+            T forward = ( (graine << 2 )  + nt) & _mask;
+            T reverse = revcomp (forward, _span);
+
+            if (forward < reverse)
+            {
+                if (this->contains (forward))
+                {
+                    edges[idx++].set (source.kmer, source.strand, forward, STRAND_FORWARD, (Nucleotide)nt, DIR_OUTCOMING);
+                }
+            }
+            else
+            {
+                if (this->contains (reverse))
+                {
+                    edges[idx++].set (source.kmer, source.strand, reverse, STRAND_REVCOMP, (Nucleotide)nt, DIR_OUTCOMING);
+                }
+            }
+        }
+    }
+    else if (direction == DIR_INCOMING)
+    {
+        /** IMPORTANT !!! Since we have hugely shift the nt value, we make sure to use a long enough integer. */
+        for (u_int64_t nt=0; nt<4; nt++)
+        {
+            T forward = ((graine >> 2 )  + ( nt << ((_span-1)*2)) ) & _mask; // previous kmer
+            T reverse = revcomp (forward, _span);
+
+            if (forward < reverse)
+            {
+                if (this->contains (forward))
+                {
+                    edges[idx++].set (forward, STRAND_FORWARD, source.kmer, source.strand, (Nucleotide)nt, DIR_INCOMING);
+                }
+            }
+            else
+            {
+                if (this->contains (reverse))
+                {
+                    edges[idx++].set (reverse, STRAND_REVCOMP, source.kmer, source.strand, (Nucleotide)nt, DIR_INCOMING);
+                }
+            }
+        }
+    }
+
+    else  {  throw system::Exception ("Unknown direction for getting edges");  }
+
+    /** We update the size of the container according to the number of found items. */
+    edges.setSize (idx);
+
+    /** We return the number of found items. */
+    return idx;
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+template<typename T>
+size_t GraphBasic<T>::getNodes (const Node<T>& source, NodeSet<T>& nodes, Direction direction)
+{
+    size_t idx = 0;
+
+    // the kmer we're extending may be actually a revcomp sequence in the bidirected debruijn graph node
+    T graine = ((source.strand == STRAND_FORWARD) ?  source.kmer : revcomp (source.kmer, _span));
+
+    if (direction == DIR_OUTCOMING)
+    {
+        for (u_int64_t nt=0; nt<4; nt++)
+        {
+            T forward = ( (graine << 2 )  + nt) & _mask;    // next kmer
+            T reverse = revcomp (forward, _span);
+
+            if (forward < reverse)  {  if (this->contains (forward))  {  nodes[idx++].set (forward, STRAND_FORWARD);  }  }
+            else                    {  if (this->contains (reverse))  {  nodes[idx++].set (reverse, STRAND_REVCOMP);  }  }
+        }
+    }
+    else if (direction == DIR_INCOMING)
+    {
+        for (u_int64_t nt=0; nt<4; nt++)
+        {
+            T forward = ((graine >> 2 )  + ( nt <<  ((_span-1)*2)) ) & _mask; // previous kmer
+            T reverse = revcomp (forward, _span);
+
+            //std::cout << "span=" << _span << "  graine=" << graine << "  nt=" << (int)nt << "  forward=" << forward << "  reverse=" << reverse << std::endl;
+
+            if (forward < reverse)  {  if (this->contains (forward))  {  nodes[idx++].set (forward, STRAND_FORWARD);  }  }
+            else                    {  if (this->contains (reverse))  {  nodes[idx++].set (reverse, STRAND_REVCOMP);  }  }
+        }
+    }
+
+    else  {  throw system::Exception ("Unknown direction for getting nodes");  }
+
+    /** We update the size of the container according to the number of found items. */
+    nodes.setSize (idx);
+
+    /** We return the number of found items. */
+    return idx;
+}
+
+/********************************************************************************/
+
+// since we didn't define the functions in a .h file, that trick removes linker errors,
+// see http://www.parashift.com/c++-faq-lite/separate-template-class-defn-from-decl.html
+
+template class GraphBasic <gatb::core::tools::math::NativeInt64>;
+#ifdef INT128_FOUND
+template class GraphBasic <gatb::core::tools::math::NativeInt128>;
+#else
+template class GraphBasic <gatb::core::tools::math::LargeInt<2> >;
+#endif
+
+template class GraphBasic <gatb::core::tools::math::LargeInt<3> >;
+template class GraphBasic <gatb::core::tools::math::LargeInt<4> >;
 
 /********************************************************************************/
 } } } } /* end of namespaces. */
--- gatb-core/src/gatb/debruijn/impl/GraphBasic.hpp
+++ gatb-core/src/gatb/debruijn/impl/GraphBasic.hpp
@@ -21,6 +21,7 @@
 #include <gatb/tools/collections/api/Iterable.hpp>
 #include <gatb/tools/collections/impl/IteratorFile.hpp>
 #include <gatb/tools/collections/impl/Bloom.hpp>
+#include <gatb/bank/api/IBank.hpp>
 #include <gatb/kmer/impl/BloomBuilder.hpp>
 #include <gatb/kmer/impl/Model.hpp>
 #include <gatb/tools/designpattern/impl/IteratorHelpers.hpp>
@@ -45,67 +46,24 @@ class GraphBasic : public IGraph<T>
 public:
 
     /** Constructor. */
-    GraphBasic (tools::misc::IProperties* props)
-        : _nodesIterable(0), _bloom(0), _cFPset(0), _model(0), _props(0)
-    {
-        setProps (props);
-
-        setNodesIterable (new tools::collections::impl::IterableFile<T> (props->getStr (STR_KMER_SOLID)));
-
-        setModel (new kmer::impl::Model<T> (props->getInt(STR_KMER_SIZE)));
-
-        /** We compute the bloom size. */
-        double lg2 = log(2);
-        float NBITS_PER_KMER = log (16*props->getInt(STR_KMER_SIZE)*(lg2*lg2))/(lg2*lg2);
-        u_int64_t bloomSize =  (u_int64_t) (system::impl::System::file().getSize (props->getStr (STR_KMER_SOLID)) / sizeof (T) * NBITS_PER_KMER);
-
-        setBloom (kmer::impl::BloomBuilder<T> (_nodesIterable->iterator(), bloomSize, 7).build ());
-
-        if (props->get(STR_KMER_CFP) != 0)
-        {
-            setcFPset (new tools::collections::impl::ContainerSet<T> (
-                new tools::collections::impl::IteratorFile<T> (props->getStr(STR_KMER_CFP))
-            ));
-        }
-
-        _mask = _model->getMask ();
-        _span = _model->getSpan ();
-    }
+    GraphBasic (tools::misc::IProperties* props);
 
     /** Constructor. */
     GraphBasic (
         tools::collections::Iterable<T>* solidKmers,
         tools::collections::Iterable<T>* cFPKmers,
         size_t kmerSize
-    )
-        : _nodesIterable(0), _bloom(0), _cFPset(0), _model(0), _props(0)
-    {
-        setNodesIterable (solidKmers);
+    );
 
-        setModel (new kmer::impl::Model<T> (kmerSize));
-
-        /** We compute the bloom size. */
-        double lg2 = log(2);
-        float NBITS_PER_KMER = log (16*kmerSize*(lg2*lg2))/(lg2*lg2);
-        u_int64_t bloomSize =  (u_int64_t) (solidKmers->getNbItems() * NBITS_PER_KMER);
-
-        setBloom (kmer::impl::BloomBuilder<T> (_nodesIterable->iterator(), bloomSize, 7).build ());
-
-        setcFPset (new tools::collections::impl::ContainerSet<T> (cFPKmers->iterator()));
-
-        _mask = _model->getMask ();
-        _span = _model->getSpan ();
-    }
+    /** Constructor. */
+    GraphBasic (
+        bank::IBank* bank,
+        size_t       kmerSize,
+        size_t       nks
+    );
 
     /** Destructor. */
-    ~GraphBasic ()
-    {
-        setProps         (0);
-        setNodesIterable (0);
-        setcFPset        (0);
-        setModel         (0);
-        setBloom         (0);
-    }
+    ~GraphBasic ();
 
     /** */
     bool contains (const Node<T>& item)  { return contains (item.kmer); }
@@ -137,18 +95,10 @@ private:
 
 
     tools::collections::Iterable<T>* _nodesIterable;
-    void setNodesIterable (tools::collections::Iterable<T>* nodesIterable)
-    {
-        //if (_nodesIterable != 0)  { delete _nodesIterable; }
-        _nodesIterable = nodesIterable;
-    }
+    void setNodesIterable (tools::collections::Iterable<T>* nodesIterable)  { SP_SETATTR(nodesIterable); }
 
     tools::collections::Container<T>* _cFPset;
-    void setcFPset (tools::collections::Container<T>* cFPset)
-    {
-        //if (_cFPset != 0)  { delete _cFPset; }
-        _cFPset = cFPset;
-    }
+    void setcFPset (tools::collections::Container<T>* cFPset)  { SP_SETATTR(cFPset); }
 
     tools::collections::impl::Bloom<T>* _bloom;
     void setBloom (tools::collections::impl::Bloom<T>* bloom)  { SP_SETATTR(bloom); }
@@ -166,113 +116,11 @@ private:
     /** */
     bool contains (const T& item)  { return _bloom->contains (item) && !_cFPset->contains(item); }
 
+    /** */
+    size_t getEdges (const Node<T>& source, EdgeSet<T>& edges, Direction direction);
 
-    /**********************************************************************/
-    size_t getEdges (const Node<T>& source, EdgeSet<T>& edges, Direction direction)
-    {
-        size_t idx = 0;
-
-        // the kmer we're extending may be actually a revcomp sequence in the bidirected debruijn graph node
-        T graine = ((source.strand == STRAND_FORWARD) ?  source.kmer : revcomp (source.kmer, _span));
-
-        if (direction == DIR_OUTCOMING)
-        {
-            for (u_int64_t nt=0; nt<4; nt++)
-            {
-                T forward = ( (graine << 2 )  + nt) & _mask;
-                T reverse = revcomp (forward, _span);
-
-                if (forward < reverse)
-                {
-                    if (this->contains (forward))
-                    {
-                        edges[idx++].set (source.kmer, source.strand, forward, STRAND_FORWARD, (Nucleotide)nt, DIR_OUTCOMING);
-                    }
-                }
-                else
-                {
-                    if (this->contains (reverse))
-                    {
-                        edges[idx++].set (source.kmer, source.strand, reverse, STRAND_REVCOMP, (Nucleotide)nt, DIR_OUTCOMING);
-                    }
-                }
-            }
-        }
-        else if (direction == DIR_INCOMING)
-        {
-            /** IMPORTANT !!! Since we have hugely shift the nt value, we make sure to use a long enough integer. */
-            for (u_int64_t nt=0; nt<4; nt++)
-            {
-                T forward = ((graine >> 2 )  + ( nt << ((_span-1)*2)) ) & _mask; // previous kmer
-                T reverse = revcomp (forward, _span);
-
-                if (forward < reverse)
-                {
-                    if (this->contains (forward))
-                    {
-                        edges[idx++].set (forward, STRAND_FORWARD, source.kmer, source.strand, (Nucleotide)nt, DIR_INCOMING);
-                    }
-                }
-                else
-                {
-                    if (this->contains (reverse))
-                    {
-                        edges[idx++].set (reverse, STRAND_REVCOMP, source.kmer, source.strand, (Nucleotide)nt, DIR_INCOMING);
-                    }
-                }
-            }
-        }
-
-        else  {  throw system::Exception ("Unknown direction for getting edges");  }
-
-        /** We update the size of the container according to the number of found items. */
-        edges.setSize (idx);
-
-        /** We return the number of found items. */
-        return idx;
-    }
-
-    /**********************************************************************/
-    size_t getNodes (const Node<T>& source, NodeSet<T>& nodes, Direction direction)
-    {
-        size_t idx = 0;
-
-        // the kmer we're extending may be actually a revcomp sequence in the bidirected debruijn graph node
-        T graine = ((source.strand == STRAND_FORWARD) ?  source.kmer : revcomp (source.kmer, _span));
-
-        if (direction == DIR_OUTCOMING)
-        {
-            for (u_int64_t nt=0; nt<4; nt++)
-            {
-                T forward = ( (graine << 2 )  + nt) & _mask;    // next kmer
-                T reverse = revcomp (forward, _span);
-
-                if (forward < reverse)  {  if (this->contains (forward))  {  nodes[idx++].set (forward, STRAND_FORWARD);  }  }
-                else                    {  if (this->contains (reverse))  {  nodes[idx++].set (reverse, STRAND_REVCOMP);  }  }
-            }
-        }
-        else if (direction == DIR_INCOMING)
-        {
-            for (u_int64_t nt=0; nt<4; nt++)
-            {
-                T forward = ((graine >> 2 )  + ( nt <<  ((_span-1)*2)) ) & _mask; // previous kmer
-                T reverse = revcomp (forward, _span);
-
-                //std::cout << "span=" << _span << "  graine=" << graine << "  nt=" << (int)nt << "  forward=" << forward << "  reverse=" << reverse << std::endl;
-
-                if (forward < reverse)  {  if (this->contains (forward))  {  nodes[idx++].set (forward, STRAND_FORWARD);  }  }
-                else                    {  if (this->contains (reverse))  {  nodes[idx++].set (reverse, STRAND_REVCOMP);  }  }
-            }
-        }
-
-        else  {  throw system::Exception ("Unknown direction for getting nodes");  }
-
-        /** We update the size of the container according to the number of found items. */
-        nodes.setSize (idx);
-
-        /** We return the number of found items. */
-        return idx;
-    }
+    /** */
+    size_t getNodes (const Node<T>& source, NodeSet<T>& nodes, Direction direction);
 
     /**********************************************************************/
     class NodeIteratorBasic : public INodeIterator<T>
