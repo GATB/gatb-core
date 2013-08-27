4738379f84e9f785077ad4e08255b931643e7ff5
edrezen <edrezen@irisa.fr>
2013-08-27 11:57:12 +0200

    cosmit

--- gatb-core/examples/debruijn4.cpp
+++ gatb-core/examples/debruijn4.cpp
@@ -60,9 +60,10 @@ int main (int argc, char* argv[])
 
     char*  bankUri  = argv[1];
     size_t kmerSize = atoi (argv[2]);
+    size_t nks      = argc >= 4 ? atoi (argv[3]) : 3;
 
     /** We create the graph with  1) a FASTA bank   2) a kmer size */
-    Graph<NativeInt64> graph = GraphFactory::createGraph <NativeInt64> (new Bank (bankUri), kmerSize);
+    Graph<NativeInt64> graph = GraphFactory::createGraph <NativeInt64> (new Bank (bankUri), kmerSize, nks);
 
     /** We get an iterator over all the nodes of the graph. */
     NodeIterator<NativeInt64> itNodes = graph.nodes();
