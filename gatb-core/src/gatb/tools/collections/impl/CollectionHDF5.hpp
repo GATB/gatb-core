0a78b110ba2d0f5a97661c86feac6e310ee13c4b
edrezen <edrezen@irisa.fr>
2013-09-20 16:34:52 +0200

    First version

---
+++ gatb-core/src/gatb/tools/collections/impl/CollectionHDF5.hpp
@@ -0,0 +1,405 @@
+/*****************************************************************************
+ *   GATB : Genome Assembly Tool Box                                         *
+ *   Authors: [R.Chikhi, G.Rizk, E.Drezen]                                   *
+ *   Based on Minia, Authors: [R.Chikhi, G.Rizk], CeCILL license             *
+ *   Copyright (c) INRIA, CeCILL license, 2013                               *
+ *****************************************************************************/
+
+/** \file Collection.hpp
+ *  \date 01/03/2013
+ *  \author edrezen
+ *  \brief Collection interface
+ *
+ *  This file holds interfaces related to the Collection interface
+ */
+
+#ifndef _GATB_CORE_TOOLS_COLLECTIONS_IMPL_COLLECTION_HDF5_HPP_
+#define _GATB_CORE_TOOLS_COLLECTIONS_IMPL_COLLECTION_HDF5_HPP_
+
+/********************************************************************************/
+
+#include <gatb/tools/collections/api/Collection.hpp>
+#include <gatb/tools/collections/impl/BagFile.hpp>
+#include <gatb/tools/collections/impl/IteratorFile.hpp>
+#include <gatb/tools/collections/impl/CollectionAbstract.hpp>
+#include <gatb/tools/designpattern/impl/IteratorHelpers.hpp>
+#include <gatb/system/impl/System.hpp>
+
+#include <string>
+#include <vector>
+#include <stdarg.h>
+#include <hdf5.h>
+
+/********************************************************************************/
+namespace gatb          {
+namespace core          {
+namespace tools         {
+namespace collections   {
+namespace impl          {
+/********************************************************************************/
+
+template <class Item> class BagHDF5 : public Bag<Item>, public dp::SmartPointer
+{
+public:
+
+    /** */
+    BagHDF5 (hid_t datasetId, hid_t typeId, u_int64_t& nbItems, system::ISynchronizer* synchro)
+        : _datasetId(datasetId), _typeId(typeId), _nbInserted(0), _nbItems(nbItems), _synchro(synchro)
+    {
+    }
+
+    /** Insert an item into the bag.
+     * \param[in] item : the item to be inserted. */
+    void insert (const Item& item)                                 {  throw "BagHDF5::insert (1)";  }
+    void insert (const std::vector<Item>& items, size_t length=0)  {  throw "BagHDF5::insert (2)";  }
+
+    /** Insert items into the bag.
+     * \param[in] items : items to be inserted. */
+    void insert (const Item* items, size_t length)
+    {
+        herr_t status = 0;
+
+        //system::LocalSynchronizer localsynchro (_synchro);
+
+        /** Resize the memory dataspace to indicate the new size of our buffer. */
+        hsize_t memDim = length;
+        hid_t memspaceId = H5Screate_simple (1, &memDim, NULL);
+
+        /** Extend dataset. */
+        hsize_t newDim = _nbInserted + length;
+        status = H5Dset_extent (_datasetId, &newDim);
+        if (status != 0)  { std::cout << "err H5Dset_extent" << std::endl; }
+
+        /** Select hyperslab on file dataset. */
+        hid_t filespaceId = H5Dget_space(_datasetId);
+        hsize_t start = _nbInserted;
+        hsize_t count = length;
+        status = H5Sselect_hyperslab (filespaceId, H5S_SELECT_SET, &start, NULL, &count, NULL);
+        if (status != 0)  { std::cout << "err H5Sselect_hyperslab" << std::endl; }
+
+        /** Append buffer to dataset */
+        status = H5Dwrite (_datasetId, _typeId, memspaceId, filespaceId, H5P_DEFAULT, items);
+        if (status != 0)  { std::cout << "err H5Dwrite" << std::endl; }
+
+        /** We increase the number of inserted items. */
+        _nbInserted += length;
+
+        __sync_fetch_and_add (&_nbItems, length);
+
+        /** Close resources. */
+        status = H5Sclose (filespaceId);
+        status = H5Sclose (memspaceId);
+        if (status != 0)  { std::cout << "err H5Sclose" << std::endl; }
+
+    }
+
+    /** */
+    void flush ()  {}
+
+private:
+
+    hid_t     _datasetId;
+    hid_t     _typeId;
+    u_int64_t& _nbItems;
+    u_int64_t _nbInserted;
+    system::ISynchronizer* _synchro;
+
+};
+
+/********************************************************************************/
+
+template <class Item> class IterableHDF5 : public Iterable<Item>, public dp::SmartPointer
+{
+public:
+
+    /** */
+    IterableHDF5 (hid_t datasetId, hid_t typeId, u_int64_t& nbItems,     system::ISynchronizer* synchro)
+        :  _datasetId(datasetId), _typeId(typeId), _nbItems(nbItems), _synchro(synchro)  {}
+
+    /** */
+    ~IterableHDF5 ()  {}
+
+    /** */
+    dp::Iterator<Item>* iterator ()  {  return new HDF5Iterator (this);  }
+
+    /** */
+    int64_t getNbItems ()
+    {
+        return _nbItems;
+    }
+
+    /** */
+    Item* getItems (Item*& buffer)
+    {
+//std::cout << "getItems: " <<getNbItems() << std::endl;
+        retrieveCache (buffer, 0, getNbItems());
+//for (size_t i=0; i<100; i++)  { printf ("%2d ", buffer[i]);  if ((i+1)%20==0)  { printf("\n"); } } printf("\n");
+        return buffer;
+    }
+
+
+private:
+
+    hid_t _datasetId;
+    hid_t _typeId;
+    u_int64_t& _nbItems;
+    system::ISynchronizer* _synchro;
+
+    /** */
+    class HDF5Iterator : public dp::Iterator<Item>
+    {
+    public:
+
+        /** */
+        HDF5Iterator (IterableHDF5* ref, size_t blockSize=4096)
+            : _ref(ref), _blockSize(blockSize),
+              _data(0), _dataSize(0), _dataIdx(0), _isDone (true),
+              _nbRead(0), _memspaceId(0), _total(0)
+        {
+            _data = (Item*) malloc (_blockSize*sizeof(Item));
+            memset (_data, 0, _blockSize*sizeof(Item));
+
+            _total = _ref->_nbItems;
+
+        }
+
+        /** */
+        ~HDF5Iterator()
+        {
+            if (_data)  { free (_data); }
+        }
+
+        void first()
+        {
+
+            _dataIdx  = 0;
+            _dataSize = retrieveNextCache();
+            _isDone   = _dataIdx >= _dataSize;
+
+            if (!_isDone)  {  *this->_item = _data[_dataIdx];  }
+        }
+
+        void next()
+        {
+            _dataIdx++;
+            _isDone = _dataIdx >= _dataSize;
+            if (!_isDone)  {  *this->_item = _data[_dataIdx];  }
+            else
+            {
+                _dataIdx  = 0;
+                _dataSize = retrieveNextCache();
+                _isDone = _dataIdx >= _dataSize;
+                if (!_isDone)  {  *this->_item = _data[_dataIdx];  }
+            }
+        }
+
+        bool isDone()
+        {
+            return _isDone;
+        }
+
+        Item& item ()  { return *this->_item; }
+
+    private:
+        IterableHDF5* _ref;
+        size_t        _blockSize;
+
+        Item*     _data;
+        u_int64_t _dataSize;
+        u_int64_t _dataIdx;
+
+        bool _isDone;
+
+        u_int64_t     _nbRead;
+        u_int64_t     _total;
+
+        hid_t _memspaceId;
+
+#if 0
+        u_int64_t retrieveNextCache ()
+        {
+            herr_t status = 0;
+
+            if (_total <= _nbRead)  {  return 0;   }
+            hsize_t nbToRead = std::min (_blockSize, _total - _nbRead);
+
+            system::LocalSynchronizer localsynchro (_ref->_synchro);
+
+            hid_t memspaceId = H5Screate_simple (1, &nbToRead, NULL);
+
+            /** Select hyperslab on file dataset. */
+            hid_t filespaceId = H5Dget_space(_ref->_datasetId);
+            hsize_t start = _nbRead;
+            hsize_t count = nbToRead;
+            status = H5Sselect_hyperslab (filespaceId, H5S_SELECT_SET, &start, NULL, &count, NULL);
+            if (status != 0)  { std::cout << "err H5Sselect_hyperslab" << std::endl; }
+
+            /** Read buffer from dataset */
+            status = H5Dread (_ref->_datasetId, _ref->_typeId, memspaceId, filespaceId, H5P_DEFAULT, _data);
+            if (status != 0)  { std::cout << "err H5Dwrite" << std::endl; }
+
+            /** Close resources. */
+            status = H5Sclose (filespaceId);
+            status = H5Sclose (memspaceId);
+            if (status != 0)  { std::cout << "err H5Sclose" << std::endl; }
+
+            _nbRead += nbToRead;
+
+            return nbToRead;
+        }
+#else
+        u_int64_t retrieveNextCache ()
+        {
+            if (_total <= _nbRead)  {  return 0;   }
+            hsize_t nbToRead = std::min (_blockSize, _total - _nbRead);
+
+            _nbRead += _ref->retrieveCache (_data, _nbRead, nbToRead);
+
+            return nbToRead;
+        }
+#endif
+
+    };
+
+    /** */
+    u_int64_t retrieveCache (Item* data, hsize_t start, hsize_t count)
+    {
+        herr_t status = 0;
+
+        system::LocalSynchronizer localsynchro (_synchro);
+
+        hid_t memspaceId = H5Screate_simple (1, &count, NULL);
+
+        /** Select hyperslab on file dataset. */
+        hid_t filespaceId = H5Dget_space(_datasetId);
+        status = H5Sselect_hyperslab (filespaceId, H5S_SELECT_SET, &start, NULL, &count, NULL);
+        if (status != 0)  { std::cout << "err H5Sselect_hyperslab" << std::endl; }
+
+        /** Read buffer from dataset */
+        status = H5Dread (_datasetId, _typeId, memspaceId, filespaceId, H5P_DEFAULT, data);
+        if (status != 0)  { std::cout << "err H5Dwrite" << std::endl; }
+
+        /** Close resources. */
+        status = H5Sclose (filespaceId);
+        status = H5Sclose (memspaceId);
+        if (status != 0)  { std::cout << "err H5Sclose" << std::endl; }
+
+        return count;
+    }
+
+    friend class HDF5Iterator;
+};
+
+/********************************************************************************/
+
+/** \brief Collection interface
+ */
+template <class Item> class CollectionHDF5 : public CollectionAbstract<Item>, public dp::SmartPointer
+{
+public:
+
+    /** Constructor. */
+    CollectionHDF5 (hid_t fileId, const std::string& filename, system::ISynchronizer* synchro)
+        : CollectionAbstract<Item> (0,0), _datasetId(0), _typeId(0), _nbItems(0)
+    {
+        herr_t status;
+
+        system::LocalSynchronizer localsynchro (synchro);
+
+        /** We get the HDF5 type of the item. */
+        bool isCompound=false;
+        _typeId = Item::hdf5(isCompound);
+
+        /** We pack the type. */
+        hid_t actualType = H5Tcopy (_typeId);
+        //if (isCompound)  {  status = H5Tpack(actualType);  }
+
+        std::string actualName = "/" + filename;
+
+        /** We look whether the object exists or not. */
+        htri_t doesExist = H5Lexists (fileId, actualName.c_str(), H5P_DEFAULT);
+
+        if (doesExist > 0)
+        {
+            _datasetId = H5Dopen2 (fileId, actualName.c_str(), H5P_DEFAULT);
+
+            hid_t filespaceId = H5Dget_space (_datasetId);
+
+            hsize_t dims;
+            H5Sget_simple_extent_dims (filespaceId, &dims, NULL);
+
+            _nbItems = dims;
+            status = H5Sclose (filespaceId);
+        }
+        else
+        {
+            /** We create the dataspace. */
+            hsize_t dims      = 0;
+            hsize_t maxdims   = H5S_UNLIMITED;
+            hid_t dataspaceId = H5Screate_simple (1, &dims, &maxdims);
+
+            /* Modify dataset creation properties, i.e. enable chunking  */
+            hsize_t chunk_dims = 4096;
+            hid_t propId = H5Pcreate     (H5P_DATASET_CREATE);
+            status       = H5Pset_layout (propId, H5D_CHUNKED);
+            status       = H5Pset_chunk  (propId, 1, &chunk_dims);
+
+            /** We create the dataset. */
+            _datasetId = H5Dcreate2 (fileId, filename.c_str(),  actualType, dataspaceId, H5P_DEFAULT, propId, H5P_DEFAULT);
+
+            /** Some cleanup. */
+            H5Pclose (propId);
+            H5Sclose (dataspaceId);
+            H5Tclose (actualType);
+        }
+
+        /** We create the bag and the iterable instances. */
+        setBag      (new BagHDF5<Item>      (_datasetId, _typeId, _nbItems, synchro));
+        setIterable (new IterableHDF5<Item> (_datasetId, _typeId, _nbItems, synchro));
+
+    }
+
+    /** Destructor. */
+    virtual ~CollectionHDF5()
+    {
+        herr_t status;
+        status = H5Dclose (_datasetId);
+        status = H5Tclose (_typeId);
+    }
+
+    /** \copydoc Collection::remove */
+    void remove ()  {}
+
+    /** \copydoc Collection::addProperty */
+    void addProperty (const std::string& key, const std::string value)
+    {
+        hid_t datatype = H5Tcopy (H5T_C_S1);  H5Tset_size (datatype, H5T_VARIABLE);
+
+        hsize_t dims = 1;
+        hid_t space_id = H5Screate_simple (1, &dims, NULL);
+
+        /** We create the attribute. */
+        hid_t attrId = H5Acreate2 (_datasetId, key.c_str(), datatype,  space_id, H5P_DEFAULT, H5P_DEFAULT);
+
+        /** We write the data. */
+        const char* array[] = { value.c_str() };
+        H5Awrite (attrId, datatype, &array);
+
+        /** We close resources. */
+        H5Aclose (attrId);
+        H5Tclose (datatype);
+        H5Sclose (space_id);
+    }
+
+private:
+
+    hid_t _datasetId;
+    hid_t _typeId;
+    u_int64_t _nbItems;
+};
+
+/********************************************************************************/
+} } } } } /* end of namespaces. */
+/********************************************************************************/
+
+#endif /* _GATB_CORE_TOOLS_COLLECTIONS_IMPL_COLLECTION_HDF5_HPP_ */
---
+++ gatb-core/src/gatb/tools/collections/impl/ProductHDF5.hpp
@@ -0,0 +1,123 @@
+/*****************************************************************************
+ *   GATB : Genome Assembly Tool Box                                         *
+ *   Authors: [R.Chikhi, G.Rizk, E.Drezen]                                   *
+ *   Based on Minia, Authors: [R.Chikhi, G.Rizk], CeCILL license             *
+ *   Copyright (c) INRIA, CeCILL license, 2013                               *
+ *****************************************************************************/
+
+/** \file Collection.hpp
+ *  \date 01/03/2013
+ *  \author edrezen
+ *  \brief Collection interface
+ *
+ *  This file holds interfaces related to the Collection interface
+ */
+
+#ifndef _GATB_CORE_TOOLS_COLLECTIONS_IMPL_PRODUCT_HDF5_HPP_
+#define _GATB_CORE_TOOLS_COLLECTIONS_IMPL_PRODUCT_HDF5_HPP_
+
+/********************************************************************************/
+
+#include <gatb/tools/collections/impl/Product.hpp>
+#include <gatb/tools/collections/impl/CollectionHDF5.hpp>
+#include <gatb/system/impl/System.hpp>
+#include <hdf5.h>
+#include <typeinfo>
+
+/********************************************************************************/
+namespace gatb          {
+namespace core          {
+namespace tools         {
+namespace collections   {
+namespace impl          {
+/********************************************************************************/
+
+class ProductHDF5Factory
+{
+public:
+
+    /** */
+    static Product<ProductHDF5Factory>* createProduct (const std::string& name, bool autoRemove)
+    {
+        return new ProductHDF5 (name, autoRemove);
+    }
+
+    /** */
+    static Group<ProductHDF5Factory>* createGroup (dp::ICell* parent, const std::string& name)
+    {
+        for ( ; parent->getParent() != 0;  parent=parent->getParent())  {}
+        ProductHDF5* product = dynamic_cast<ProductHDF5*> (parent);
+
+        /** We create the instance. */
+        Group<ProductHDF5Factory>* result = new Group<ProductHDF5Factory> (parent, name);
+
+        /** We create the HDF5 group. */
+        hid_t group = H5Gcreate (product->getId(), result->getFullId('/').c_str(), H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        H5Gclose (group);
+
+        /** We return the result. */
+        return result;
+    }
+
+    /** */
+    template<typename Type>
+    static Partition<ProductHDF5Factory,Type>* createPartition (dp::ICell* parent, const std::string& name, size_t nb)
+    {
+        return new Partition<ProductHDF5Factory,Type> (parent, name, nb);
+    }
+
+    /** */
+    template<typename Type>
+    static CollectionNode<Type>* createCollection (dp::ICell* parent, const std::string& name, system::ISynchronizer* synchro)
+    {
+        for ( ; parent->getParent() != 0;  parent=parent->getParent())  {}
+        ProductHDF5* product = dynamic_cast<ProductHDF5*> (parent);
+
+        return new CollectionNode<Type> (parent, name, new CollectionHDF5<Type>(product->getId(), name, synchro));
+    }
+
+private:
+
+    class ProductHDF5 : public Product<ProductHDF5Factory>
+    {
+    public:
+        ProductHDF5 (const std::string& name, bool autoRemove) : Product<ProductHDF5Factory> (name, autoRemove), _fileId(0), _name(name)
+        {
+            /** We create a new file using default properties. */
+            if (system::impl::System::file().doesExist(getActualName()))
+            {
+                _fileId = H5Fopen (getActualName().c_str(), H5P_DEFAULT, H5P_DEFAULT);
+            }
+            else
+            {
+                _fileId = H5Fcreate (getActualName().c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+            }
+        }
+
+        virtual ~ProductHDF5 ()
+        {
+            if (_autoRemove)  { remove(); }
+            H5Fclose(_fileId);
+        }
+
+        hid_t getId ()  { return _fileId; }
+
+        void remove ()
+        {
+            system::impl::System::file().remove (getActualName());
+        }
+
+    private:
+        hid_t       _fileId;
+        std::string _name;
+
+        std::string getActualName ()  { return _name + ".h5"; }
+
+    };
+};
+
+/********************************************************************************/
+} } } } } /* end of namespaces. */
+/********************************************************************************/
+
+#endif /* _GATB_CORE_TOOLS_COLLECTIONS_IMPL_PRODUCT_HDF5_HPP_ */
