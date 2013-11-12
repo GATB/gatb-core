615982ea7d9bfceb28803b1fbea81e48d624399f
edrezen <edrezen@irisa.fr>
2013-11-12 10:19:14 +0100

    first version

---
+++ gatb-core/src/gatb/debruijn/impl/GraphHelpers.cpp
@@ -0,0 +1,143 @@
+/*****************************************************************************
+ *   GATB : Genome Assembly Tool Box                                         *
+ *   Authors: [R.Chikhi, G.Rizk, E.Drezen]                                   *
+ *   Based on Minia, Authors: [R.Chikhi, G.Rizk], CeCILL license             *
+ *   Copyright (c) INRIA, CeCILL license, 2013                               *
+ *****************************************************************************/
+
+#include <gatb/debruijn/impl/GraphHelpers.hpp>
+
+using namespace std;
+
+
+#define DEBUG(a)  //printf a
+
+/********************************************************************************/
+namespace gatb {  namespace core {  namespace debruijn {  namespace impl {
+/********************************************************************************/
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+int GraphHelper::simplePathAvance (const Node& node, Direction dir) const
+{
+    Edge output;  return simplePathAvance (node, dir, output);
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+int GraphHelper::simplePathAvance (const Node& node, Direction dir, Edge& output) const
+{
+    Graph::Vector<Edge> neighbors = _graph.neighbors<Edge> (node, dir);
+
+    /** We check we have no outbranching. */
+    if (neighbors.size() == 1)
+    {
+        /** We check whether the neighbor has an inbranching or not. */
+        if (_graph.degree (neighbors[0].to, reverse(dir)) > 1)  { return -2; }
+
+        output = neighbors[0];
+        return 1;
+    }
+
+    /** if this kmer has out-branching, don't extend it. */
+    if (neighbors.size() > 1)  {  return -1;  }
+
+    return 0;
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+Graph::Iterator<Node> GraphHelper::getSimplePathNodeIterator (const Node& node, Direction dir) const
+{
+    class LocalIterator : public tools::dp::ISmartIterator<Node>
+    {
+    public:
+
+        LocalIterator (const GraphHelper& helper, const Node& node, Direction dir)
+            : _helper(helper), _graph(helper.getGraph()), _dir(dir), _isDone(true)
+        {
+            *(this->_item) = node;
+        }
+
+        /** \copydoc  Iterator::first */
+        void first()
+        {
+            _isDone = false;
+            next ();
+        }
+
+        /** \copydoc  Iterator::next */
+        void next()
+        {
+            Edge output;
+
+            if (_helper.simplePathAvance (*(this->_item), _dir, output) > 0)
+            {
+                /** We update the currently iterated node. */
+                *(this->_item) = output.to;
+            }
+            else
+            {
+                /** We can't have a non branching node in the wanted direction => iteration is finished. */
+                _isDone = true;
+            }
+//
+//            Graph::Vector<Node> neighbors = _graph.neighbors<Node> (*(this->_item), _dir);
+//
+//            if (neighbors.size() == 1)
+//            {
+//                if (_graph.degree (neighbors[0], reverse(_dir)) > 1)
+//                {
+//                    _isDone=true;
+//                }
+//                else
+//                {
+//                    *(this->_item) = neighbors[0];
+//                }
+//            }
+//            else
+//            {
+//                _isDone = true;
+//            }
+        }
+
+        /** \copydoc  Iterator::isDone */
+        bool isDone() {  return _isDone;  }
+
+        /** \copydoc  Iterator::item */
+        Node& item ()  {  return *(this->_item);  }
+
+        /** */
+        u_int64_t getNbItems () const { return 0; }
+
+    private:
+        const GraphHelper& _helper;
+        const Graph&       _graph;
+        Direction          _dir;
+        bool               _isDone;
+    };
+
+    return Graph::Iterator<Node> (new LocalIterator(*this, node, dir));
+}
+
+/********************************************************************************/
+} } } } /* end of namespaces. */
+/********************************************************************************/
---
+++ gatb-core/src/gatb/debruijn/impl/GraphHelpers.hpp
@@ -0,0 +1,70 @@
+/*****************************************************************************
+ *   GATB : Genome Assembly Tool Box                                         *
+ *   Authors: [R.Chikhi, G.Rizk, E.Drezen]                                   *
+ *   Based on Minia, Authors: [R.Chikhi, G.Rizk], CeCILL license             *
+ *   Copyright (c) INRIA, CeCILL license, 2013                               *
+ *****************************************************************************/
+
+/** \file GraphHelpers.hpp
+ *  \date 01/03/2013
+ *  \author edrezen
+ *  \brief
+ */
+
+#ifndef _GATB_CORE_DEBRUIJN_IMPL_GRAPH_HELPERS_HPP_
+#define _GATB_CORE_DEBRUIJN_IMPL_GRAPH_HELPERS_HPP_
+
+/********************************************************************************/
+
+#include <gatb/debruijn/impl/Graph.hpp>
+
+/********************************************************************************/
+namespace gatb      {
+namespace core      {
+namespace debruijn  {
+namespace impl      {
+
+/********************************************************************************/
+
+class GraphHelper
+{
+public:
+
+    /** */
+    GraphHelper (const Graph& graph) : _graph(graph) {}
+
+    /** */
+    const Graph& getGraph() const { return _graph; }
+
+    // simple paths traversal
+    // invariant: the input kmer has no in-branching.
+    // returns:
+    //      1 if a good extension is found
+    //      0 if a deadend was reached
+    //     -1 if out-branching was detected
+    //     -2 if no out-branching but next kmer has in-branching
+    int simplePathAvance (const Node& node, Direction dir) const;
+    int simplePathAvance (const Node& node, Direction dir, Edge& output) const;
+
+    /** */
+    template<typename T> Graph::Iterator<T> simplePathIterator (const Node& node, Direction dir) const;
+
+private:
+
+    /** */
+    const Graph& _graph;
+
+    /** */
+    Graph::Iterator<Node> getSimplePathNodeIterator (const Node& node, Direction dir) const;
+};
+
+/********************************************************************************/
+
+template<> inline Graph::Iterator<Node> GraphHelper::simplePathIterator (const Node& node, Direction dir) const
+{  return getSimplePathNodeIterator (node, dir);  }
+
+/********************************************************************************/
+} } } } /* end of namespaces. */
+/********************************************************************************/
+
+#endif /* _GATB_CORE_DEBRUIJN_IMPL_GRAPH_HELPERS_HPP_ */
