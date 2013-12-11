f66ef87ed9a486e2a706cc074587ec90f918aa8d
edrezen <edrezen@irisa.fr>
2013-12-11 13:36:59 +0100

    simplification

--- gatb-core/examples/debruijn/debruijn14.cpp
+++ gatb-core/examples/debruijn/debruijn14.cpp
@@ -3,9 +3,6 @@
 // We include what we need for the test
 #include <gatb/gatb_core.hpp>
 
-#undef NDEBUG
-#include <cassert>
-
 /********************************************************************************/
 /*                          Simple path iteration (with Node)                   */
 /********************************************************************************/
@@ -22,7 +19,7 @@ int main (int argc, char* argv[])
     // We get the first node of the sequence.
     Node node = graph.buildNode (seq);
 
-    // We create a Node iterator that iterates all the simple node from the first node
+    // We create a Node iterator that iterates all the simple nodes from the first node
     // Remember that a simple node has indegree==1 and outdegree==1
     Graph::Iterator<Node> path = graph.simplePath<Node> (node, DIR_OUTCOMING);
 
@@ -30,20 +27,9 @@ int main (int argc, char* argv[])
     for (path.first(); !path.isDone(); path.next())
     {
         std::cout << "   [" << path.rank() << "]  current item is " << graph.toString (path.item()) << std::endl;
-
-        // We extract the sequence substring of length kmersize, starting at ith position
-        // Remark: the Node iterator here doesn't hold the initial user node, we have therefore to begin the check
-        // beginning at offset 1 (which explains the +1)
-        std::string subseq (seq, path.rank()+1, graph.getKmerSize());
-
-        // We check that the current simple node matches the sequence at the correct position.
-        assert (graph.toString (path.item()) == subseq);
     }
 
     std::cout << "The simple path was " << path.rank() << " long" << std::endl;
-
-    // We check that we found the correct number of nodes during the simple path iteration
-    assert (path.rank() == strlen(seq) - graph.getKmerSize());
 
     return EXIT_SUCCESS;
 }
--- gatb-core/examples/debruijn/debruijn15.cpp
+++ gatb-core/examples/debruijn/debruijn15.cpp
@@ -3,9 +3,6 @@
 // We include what we need for the test
 #include <gatb/gatb_core.hpp>
 
-#undef NDEBUG
-#include <cassert>
-
 /********************************************************************************/
 /*                          Simple path iteration (with Edge)                   */
 /********************************************************************************/
@@ -22,7 +19,7 @@ int main (int argc, char* argv[])
     // We get the first node of the sequence.
     Node node = graph.buildNode (seq);
 
-    // We create a Edge iterator that iterates all the simple node from the first node
+    // We create a Edge iterator that iterates all the simple edges from the first node
     // Recall that a simple node has indegree==1 and outdegree==1
     Graph::Iterator<Edge> path = graph.simplePath<Edge> (node, DIR_OUTCOMING);
 
@@ -30,15 +27,9 @@ int main (int argc, char* argv[])
     for (path.first(); !path.isDone(); path.next())
     {
         std::cout << "   [" << path.rank() << "]  current item is " << graph.toString (path.item()) << std::endl;
-
-        // We check that the transition nucleotide matches the correct character in the sequence
-        assert (ascii(path.item().nt) == seq [graph.getKmerSize() + path.rank()]);
     }
 
     std::cout << "The simple path was " << path.rank() << " long" << std::endl;
-
-    // We check that we found the correct number of nodes during the simple path iteration
-    assert (path.rank() == strlen(seq) - graph.getKmerSize());
 
     return EXIT_SUCCESS;
 }
