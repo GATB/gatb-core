2d3c3b8482b531ce4c585627bd09541758489c41
edrezen <edrezen@irisa.fr>
2013-12-12 08:49:48 +0100

    first version

---
+++ gatb-core/doc/doxygen/src/downloadpage.hpp
@@ -0,0 +1,31 @@
+/*****************************************************************************
+ *   GATB : Genome Assembly Tool Box                                         *
+ *   Copyright (c) 2013                                                      *
+ *                                                                           *
+ *   GATB is free software; you can redistribute it and/or modify it under   *
+ *   the CECILL version 2 License, that is compatible with the GNU General   *
+ *   Public License                                                          *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   CECILL version 2 License for more details.                              *
+ *****************************************************************************/
+
+/**\page download_page Download
+ *
+ ****************************************************************************************************
+ ****************************************************************************************************
+ * \section download Download archives
+ *
+ * You can download the gatb-core archive for your system:
+ *  - <a href="https://gforge.inria.fr/frs/download.php/33247/gatb-core-1.0.1-Linux.tar.gz">gatb-core for linux</a>
+ *  - <a href="https://gforge.inria.fr/frs/download.php/33248/gatb-core-1.0.1-Darwin.tar.gz">gatb-core for macos</a>
+ *
+ * This archive holds:
+ *  - the library
+ *  - the header files
+ *  - some binaries
+ *  - some snippets
+ *
+ */
