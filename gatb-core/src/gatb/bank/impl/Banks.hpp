435a254fc58cdc1739fb228102a641c00d0254fc
edrezen <edrezen@irisa.fr>
2014-01-07 17:33:46 +0100

    first version

---
+++ gatb-core/src/gatb/bank/impl/Banks.hpp
@@ -0,0 +1,20 @@
+/*****************************************************************************
+ *   GATB : Genome Assembly Tool Box                                         *
+ *   Authors: [R.Chikhi, G.Rizk, E.Drezen]                                   *
+ *   Based on Minia, Authors: [R.Chikhi, G.Rizk], CeCILL license             *
+ *   Copyright (c) INRIA, CeCILL license, 2013                               *
+ *****************************************************************************/
+
+/** \file Banks.hpp
+ *  \date 01/03/2013
+ *  \author edrezen
+ *  \brief Include all IBank implementations
+ */
+
+/********************************************************************************/
+
+#include <gatb/bank/impl/BankFasta.hpp>
+#include <gatb/bank/impl/BankBinary.hpp>
+#include <gatb/bank/impl/BankStrings.hpp>
+#include <gatb/bank/impl/BankSplitter.hpp>
+#include <gatb/bank/impl/BankRandom.hpp>
