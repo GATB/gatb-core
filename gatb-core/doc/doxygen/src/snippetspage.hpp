ba7195b689ec337b4e9b14abc036473eb32c33da
edrezen <edrezen@irisa.fr>
2014-01-14 12:16:54 +0100

    Modify headers of *.hpp and *.cpp => Affero GPL

--- gatb-core/doc/doxygen/src/snippetspage.hpp
+++ gatb-core/doc/doxygen/src/snippetspage.hpp
@@ -1,16 +1,20 @@
 /*****************************************************************************
- *   GATB : Genome Assembly Tool Box                                         *
- *   Copyright (c) 2013                                                      *
- *                                                                           *
- *   GATB is free software; you can redistribute it and/or modify it under   *
- *   the CECILL version 2 License, that is compatible with the GNU General   *
- *   Public License                                                          *
- *                                                                           *
- *   This program is distributed in the hope that it will be useful,         *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
- *   CECILL version 2 License for more details.                              *
- *****************************************************************************/
+ *   GATB : Genome Assembly Tool Box
+ *   Copyright (C) 2014  R.Chikhi, G.Rizk, E.Drezen
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*****************************************************************************/
 
 /**\page snippets_page How to use the library ?
  *
