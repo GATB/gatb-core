fd1dbc5d8799e5d9577a7c2ba810b9599f3b0f01
edrezen <edrezen@irisa.fr>
2014-01-31 18:02:00 +0100

    Redesign of the example

--- gatb-core/examples/bank/bank10.cpp
+++ gatb-core/examples/bank/bank10.cpp
@@ -7,69 +7,103 @@
 // We use the required packages
 using namespace std;
 
+static const char* STR_BANKS_NB       = "-split";
+static const char* STR_MAX_INPUT_SIZE = "-max-size";
+
 /********************************************************************************/
 /*                         Bank conversion to binary format                     */
 /********************************************************************************/
 int main (int argc, char* argv[])
 {
-    if (argc < 3)
-    {
-        cerr << "you must provide an input and output banks names:" << endl;
-        cerr << "   1) input URI"  << endl;
-        cerr << "   2) nb sub banks" << endl;
-        return EXIT_FAILURE;
-    }
+    /** We create a command line parser. */
+    OptionsParser parser ("BankSplitter");
+    parser.push_back (new OptionOneParam (STR_URI_INPUT,      "bank reference",       true));
+    parser.push_back (new OptionOneParam (STR_BANKS_NB,       "number of sub banks",  true));
+    parser.push_back (new OptionOneParam (STR_MAX_INPUT_SIZE, "average db size per split", false, "10000000"));
+    parser.push_back (new OptionOneParam (STR_URI_OUTPUT_DIR, "output directory",     false, "."));
 
     // We define a try/catch block in case some method fails (bad filename for instance)
     try
     {
-        // We declare an input Bank
-        BankFasta inputBank (argv[1]);
+        /** We parse the user options. */
+        IProperties* options = parser.parse (argc, argv);
 
-        // We declare an output Bank
-        int nbBanks = atoi (argv[2]);
-        if (nbBanks <= 0)  { throw Exception ("You should provide a positive number of splits"); }
+        /** Shortcuts. */
+        u_int64_t maxDbSize = options->getInt(STR_MAX_INPUT_SIZE);
+
+        // We declare an input Bank
+        IBank* inputBank = BankRegistery::singleton().getFactory()->createBank (options->getStr(STR_URI_INPUT));
+        LOCAL (inputBank);
 
         // We get the basename of the input bank.
-        string bankName = System::file().getBaseName(argv[1]);
+        string inputBasename = System::file().getBaseName (options->getStr(STR_URI_INPUT));
 
-        // We create the N output banks.
-        vector<IBank*> banks (nbBanks);
-        for (size_t i=0; i<banks.size(); i++)
-        {
-            stringstream ss;  ss << bankName << "_" << i << ".fa";
-            banks[i] = BankRegistery::singleton().getFactory()->createBank (ss.str());
-        }
+        int nbBanks = options->getInt (STR_BANKS_NB);
+        if (nbBanks <= 0)  { throw Exception ("You should provide a positive number of splits"); }
+
+        /** We set the name of the output directory. */
+        stringstream ss;  ss << inputBasename << "_S" << maxDbSize << "_N" << nbBanks;
+        string outputDirName = ss.str();
+
+        /** We create the output directory. */
+        string outputDir = options->getStr(STR_URI_OUTPUT_DIR) + "/" + outputDirName;
+        System::file().mkdir (outputDir, S_IRWXU);
+
+        // We create the album bank.
+        BankAlbum album (outputDir + "/album.txt");
 
         // We create a sequence iterator for the bank
-        Iterator<Sequence>* itInput = inputBank.iterator();
+        Iterator<Sequence>* itInput = inputBank->iterator();
+
+        /** We get estimations about the bank. */
+        u_int64_t number, totalSize, maxSize;
+        inputBank->estimate (number, totalSize, maxSize);
+
+        u_int64_t estimationNbSeqToIterate = (number*maxDbSize*nbBanks) / totalSize;
 
         // We create an iterator over the input bank and encapsulate it with progress notification.
-        SubjectIterator<Sequence> itSeq1 (itInput, 100000);
-        itSeq1.addObserver (new ProgressTimer (inputBank.estimateNbSequences(), "count"));
+        SubjectIterator<Sequence> itSeq (itInput, 1000);
+        itSeq.addObserver (new ProgressTimer (estimationNbSeqToIterate, "split"));
 
         // We loop over sequences to get the exact number of sequences.
-        size_t nbSequences = 0;
-        for (itSeq1.first(); !itSeq1.isDone(); itSeq1.next())  { nbSequences ++; }
+          int64_t nbBanksOutput = -1;
+        u_int64_t nbSequences   =  0;
+        u_int64_t dbSize        = ~0;
 
-        // We create an iterator over the input bank and encapsulate it with progress notification.
-        SubjectIterator<Sequence> itSeq2 (itInput, 100000);
-        itSeq2.addObserver (new ProgressTimer (inputBank.estimateNbSequences(), "split"));
+        IBank* currentBank = 0;
 
-        // We loop over sequences and put them into the correct split bank.
-        for (itSeq2.first(); !itSeq2.isDone(); itSeq2.next())
+        for (itSeq.first(); !itSeq.isDone(); itSeq.next())
         {
-            size_t id = (*itSeq2).getIndex() / (nbSequences / nbBanks);
-            banks[id]->insert (*itSeq2);
+            if (dbSize > maxDbSize)
+            {
+                if (currentBank != 0)  { currentBank->flush(); }
+
+                nbBanksOutput ++;
+                if (nbBanksOutput > nbBanks)  { break; }
+
+                /** We build the uri of the current bank. */
+                stringstream ss;  ss << inputBasename << "_" << nbBanksOutput;
+
+                /** We create a new bank and put it in the album. */
+                currentBank = album.addBank (outputDir, ss.str());
+
+                /** We reinit the db size counter. */
+                dbSize = 0;
+            }
+
+            dbSize += itSeq->getDataSize();
+
+            /** We insert the sequence into the current output bank. */
+            currentBank->insert (*itSeq);
         }
 
-        // Some clean up
-        for (size_t i=0; i<banks.size(); i++)
-        {
-            banks[i]->flush();
-            delete banks[i];
-        }
-
+        if (currentBank != 0)  { currentBank->flush(); }
+    }
+    catch (OptionFailure& e)
+    {
+        e.getParser().displayErrors (stdout);
+        e.getParser().displayHelp   (stdout);
+        return EXIT_FAILURE;
     }
     catch (gatb::core::system::Exception& e)
     {
