f5ba951b41e0f7068dc6a09e8f719fadb82fe460
edrezen <edrezen@gaaz.irisa.fr>
2014-02-25 16:56:16 +0100

    use static library for cppunit

--- gatb-core/cmake/CppUnit.cmake
+++ gatb-core/cmake/CppUnit.cmake
@@ -3,9 +3,11 @@
 ################################################################################
 
 FIND_PATH (CPPUNIT_INCLUDE_DIR cppunit/extensions/HelperMacros.h
-  /local/users/edrezen/local/include
+  /local/include
   /usr/include
+  /usr/local/Cellar/cppunit/1.12.1/include
 )
+SET (CPPUNIT_INCLUDE_DIR "/usr/local/Cellar/cppunit/1.12.1/include")
 
 FIND_LIBRARY (CPPUNIT_LIBRARY cppunit
     ${CPPUNIT_INCLUDE_DIR}/../lib
@@ -15,6 +17,7 @@ IF (CPPUNIT_INCLUDE_DIR)
     IF (CPPUNIT_LIBRARY)
         SET (CPPUNIT_FOUND "YES")
         SET (CPPUNIT_LIBRARIES ${CPPUNIT_LIBRARY})
+        SET (CPPUNIT_LIBRARY_STATIC ${CPPUNIT_INCLUDE_DIR}/../lib/libcppunit.a)
     ENDIF (CPPUNIT_LIBRARY)
 ENDIF (CPPUNIT_INCLUDE_DIR)
 
