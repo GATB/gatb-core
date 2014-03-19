686b0553a5a64660a1111cefb4e465c8e4edb19a
edrezen <edrezen@irisa.fr>
2014-03-19 17:31:18 +0100

    Update bloom and debloom strings

--- gatb-core/src/gatb/tools/misc/api/StringsRepository.hpp
+++ gatb-core/src/gatb/tools/misc/api/StringsRepository.hpp
@@ -97,8 +97,8 @@ public:
     const char* help           ()  { return "-help";           }
     const char* output_format  ()  { return "-outfmt";         }
     const char* version        ()  { return "-version";        }
-    const char* bloom_type     ()  { return "-bloom-type";     }
-    const char* cfp_type       ()  { return "-cfp-type";       }
+    const char* bloom_type     ()  { return "-bloom";          }
+    const char* debloom_type   ()  { return "-debloom";        }
 };
 
 /********************************************************************************/
@@ -126,7 +126,7 @@ public:
 #define STR_OUTPUT_FORMAT       gatb::core::tools::misc::StringRepository::singleton().output_format ()
 #define STR_VERSION             gatb::core::tools::misc::StringRepository::singleton().version ()
 #define STR_BLOOM_TYPE          gatb::core::tools::misc::StringRepository::singleton().bloom_type()
-#define STR_CFP_TYPE            gatb::core::tools::misc::StringRepository::singleton().cfp_type()
+#define STR_DEBLOOM_TYPE        gatb::core::tools::misc::StringRepository::singleton().debloom_type()
 
 /********************************************************************************/
 
