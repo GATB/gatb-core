963ad3af1259310c67677f840aea4e6dde7d5a1d
edrezen <edrezen@irisa.fr>
2014-03-21 09:41:50 +0100

    initial guess moved to storage6

--- gatb-core/examples/storage/storage5.cpp
+++ gatb-core/examples/storage/storage5.cpp
@@ -27,30 +27,11 @@ int main (int argc, char* argv[])
     // We get the solid kmers collection 1) from the 'dsk' group  2) from the 'solid' collection
     Collection<Kmer<>::Count>& solidKmers = storage->getGroup("dsk").getCollection<Kmer<>::Count> ("solid");
 
-    // We can retrieve information (as an XML string) about the construction of the solid kmers
-    cout << solidKmers.getProperty("properties") << endl;
-
-    // We can access each of these information through a Properties object
-    Properties props;
-    props.readXML (solidKmers.getProperty("properties"));
-
-    // Now, we can for instance get the kmer size (as an integer)
-    cout << "kmer size:      " << props.getInt ("kmer_size")      << endl;
-    cout << "nb solid kmers: " << props.getInt ("kmers_nb_solid") << endl;
-
-    // We create a Model instance. It will help to dump the kmers in
-    // a human readable form (ie as a string of nucleotides)
-    Kmer<>::Model model (props.getInt ("kmer_size"));
-
     // We iterate (through a lambda expression) the solid kmers from the retrieved collection
-    size_t nbKmers = 0;
     solidKmers.iterate ([&] (const Kmer<>::Count& count)
     {
-        // We dump the solid kmer information:
-        //   1) nucleotides
-        //   2) raw value (integer)
-        //   3) abundance
-        cout << "[" << ++nbKmers << "]  " << model.toString(count.value) << "  " << count.value << "  "  << count.abundance << endl;
+        // We dump the solid kmer information (kmer value and abundance)
+        cout << count.value << "  "  << count.abundance << endl;
     });
 }
 //! [snippet1]
