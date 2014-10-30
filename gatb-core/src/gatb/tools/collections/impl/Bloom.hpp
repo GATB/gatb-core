f5d9f276f6df9b1dc3deafcc48512f30f41b420a
Guillaume Rizk <guillaume.rizk@gmail.com>
2014-10-30 20:48:47 +0100

    debug

--- gatb-core/src/gatb/tools/collections/impl/Bloom.hpp
+++ gatb-core/src/gatb/tools/collections/impl/Bloom.hpp
@@ -330,7 +330,7 @@ public:
      * \param[in] tai_bloom : size (in bits) of the bloom filter.
      * \param[in] nbHash : number of hash functions to use
      * \param[in] block_nbits : size of the block (actual 2^nbits) */
-    BloomCacheCoherent (u_int64_t tai_bloom, size_t nbHash = 4,size_t block_nbits = 12)  : Bloom<Item> (tai_bloom + (1<<block_nbits), nbHash),_nbits_BlockSize(block_nbits)
+    BloomCacheCoherent (u_int64_t tai_bloom, size_t nbHash = 4,size_t block_nbits = 12)  : Bloom<Item> (tai_bloom + 2*(1<<block_nbits), nbHash),_nbits_BlockSize(block_nbits)
     {
         _mask_block = (1<<_nbits_BlockSize) - 1;
         _reduced_tai = this->tai -  2*(1<<_nbits_BlockSize) ;//2* for neighbor coherent
