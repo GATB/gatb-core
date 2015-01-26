ebaec934a7dc622a914a26291469faa9923842cc
edrezen <edrezen@irisa.fr>
2015-01-26 10:40:57 +0100

    Backward compatibility with old hdf5 files.

--- gatb-core/src/gatb/tools/storage/impl/StorageHDF5.hpp
+++ gatb-core/src/gatb/tools/storage/impl/StorageHDF5.hpp
@@ -95,14 +95,18 @@ public:
         /** If the nb of partitions is null, we try to get it from a property. */
         if (nb==0)
         {
-            std::string nbPartStr = group.getProperty ("nb_partitions");
-            if (nbPartStr.empty()==false)  { nb = atoi (nbPartStr.c_str()); }
-            else                           { throw system::Exception ("Partition '%s' has 0 items", name.c_str()); }
+            nb = StorageHDF5Factory::getPartitionsNb (&group);
+
+            /** For backward compatibility only (ugly) : we look for the attribute in the parent object. */
+            if (nb==0)  {  nb = StorageHDF5Factory::getPartitionsNb (dynamic_cast<Group*> (parent));  }
+
+            /** Ok, we can't find any value. */
+            if (nb==0)  {  throw system::Exception ("Partition '%s' has 0 items", name.c_str());      }
         }
         else
         {
             std::stringstream ss; ss << nb;
-            group.addProperty ("nb_partitions", ss.str());
+            group.addProperty (StorageHDF5Factory::getNbPartitionsName(), ss.str());
         }
 
         return new Partition<Type> (storage->getFactory(), parent, name, nb);
@@ -142,6 +146,23 @@ private:
         ~GlobalSynchro () { if (synchro)  { delete synchro; } }
         system::ISynchronizer* synchro;
     };
+
+    /* */
+    static const char* getNbPartitionsName()  { return "nb_partitions"; }
+
+    /** Get the number of partitions (if any).
+     * \param[in] group : check the attribute in this group
+     * \return 0 if no attribute, otherwise the number of partitions */
+    static size_t getPartitionsNb (Group* group)
+    {
+        size_t result = 0;
+        if (group != 0)
+        {
+            std::string nbPartStr = group->getProperty (getNbPartitionsName());
+            if (nbPartStr.empty()==false)  { result = atoi (nbPartStr.c_str()); }
+        }
+        return result;
+    }
 
     /************************************************************/
     class StorageHDF5 : public Storage
@@ -271,30 +292,34 @@ private:
             std::string result;
             herr_t status;
 
-            hid_t datatype = H5Tcopy (H5T_C_S1);  H5Tset_size (datatype, H5T_VARIABLE);
-
-            hid_t attrId = H5Aopen (_groupId, key.c_str(), H5P_DEFAULT);
-            if (attrId >= 0)
+            /** We first check that the attribute exitst. */
+            if ( H5Aexists(_groupId, key.c_str()) > 0)
             {
-                hid_t space_id = H5Aget_space (attrId);
+                hid_t datatype = H5Tcopy (H5T_C_S1);  H5Tset_size (datatype, H5T_VARIABLE);
 
-                hsize_t dims = 1;
-                H5Sget_simple_extent_dims (space_id, &dims, NULL);
-                char** rdata = (char **) MALLOC (dims * sizeof (char *));
+                hid_t attrId = H5Aopen (_groupId, key.c_str(), H5P_DEFAULT);
+                if (attrId >= 0)
+                {
+                    hid_t space_id = H5Aget_space (attrId);
 
-                status = H5Aread (attrId, datatype, rdata);
+                    hsize_t dims = 1;
+                    H5Sget_simple_extent_dims (space_id, &dims, NULL);
+                    char** rdata = (char **) MALLOC (dims * sizeof (char *));
 
-                /** We set the result. */
-                result.assign (rdata[0]);
+                    status = H5Aread (attrId, datatype, rdata);
 
-                /** We release buffers. */
-                status = H5Dvlen_reclaim (datatype, space_id, H5P_DEFAULT, rdata);
-                FREE (rdata);
+                    /** We set the result. */
+                    result.assign (rdata[0]);
 
-                /** We close resources. */
-                H5Aclose (attrId);
-                H5Tclose (datatype);
-                H5Sclose (space_id);
+                    /** We release buffers. */
+                    status = H5Dvlen_reclaim (datatype, space_id, H5P_DEFAULT, rdata);
+                    FREE (rdata);
+
+                    /** We close resources. */
+                    H5Aclose (attrId);
+                    H5Tclose (datatype);
+                    H5Sclose (space_id);
+                }
             }
 
             return result;
