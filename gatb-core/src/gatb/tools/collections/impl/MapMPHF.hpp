2dbc7eeece99ae0d43f241a41ad57f24ba03483a
Rayan Chikhi <rayan.chikhi@ens-cachan.org>
2015-05-08 15:37:49 +0200

    minor mphf fix

--- gatb-core/src/gatb/tools/collections/impl/MapMPHF.hpp
+++ gatb-core/src/gatb/tools/collections/impl/MapMPHF.hpp
@@ -89,6 +89,8 @@ public:
         
         /** We resize the vector of Value objects. */
         data.resize ((hash.size() / x) + 1); // that +1 and not (hash.size+x-1) / x
+
+        clearData();
     }
 
     /** Save the hash function into a Group object.
@@ -128,7 +130,10 @@ public:
      * \return keys number. */
     size_t size() const { return hash.size(); }
 
-    void clearData() { data.clear(); }
+    void clearData() { 
+        for (unsigned long i = 0; i < data.size(); i ++)
+            data[i] = 0;
+     }
 
 private:
 
