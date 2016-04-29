2e2920026181fb83e4d729018e34a32edd2db12a
Charles DELTEL <charles.deltel@inria.fr>
2016-04-29 16:07:45 +0200

    [ci] new jenkins item for compiling snippets

--- gatb-core/test/jenkins/test-snippets-debian7-64bits-gcc-4.7.sh
+++ gatb-core/test/jenkins/test-snippets-debian7-64bits-gcc-4.7.sh
@@ -2,11 +2,7 @@
 #--------------------------------------------------------------#
 #         Continuous integration script for Jenkins            #
 #--------------------------------------------------------------#
-#
-# Default mode :
-# This script will exit with error (exit code 1) if any of its steps fails.
-# To change this behaviour, choose DO_NOT_STOP_AT_ERROR in Jenkins (see below).
-#--------------------------------------------------------------#
+
 set +xv
 
 echo "
@@ -34,10 +30,7 @@ BUILD_NUMBER         : ${BUILD_NUMBER}
 
 "
 
-error_code () { [ "$DO_NOT_STOP_AT_ERROR" = "true" ] && { return 0 ; } }
 
-
-[ "$DO_NOT_STOP_AT_ERROR" != "true" ] && { set -e ; } || { echo "(!) DEBUG mode, the script will NOT stop..." ; echo; }
 set -xv
 
 # quick look at resources
@@ -75,42 +68,21 @@ cmake -Wno-dev $GIT_DIR
 make -j 2 || error_code
 
 ################################################################
-#                   VALGRIND CHECKS                            #
+#                   COMPILE SNIPPETS                           #
 ################################################################
 
+cd $BUILD_DIR/examples
 
-#>>>>>>>>>>>> 1. Diagnostics for TestDebruijn >>>>>>>>>>>>>>>>>>
+make
 
-export CPPUNIT_VERBOSE=1
+################################################################
+#                      RUN SNIPPETS                            #
+################################################################
 
-# Copy database for unit tests
-cp -r $GIT_DIR/test/db $BUILD_DIR/test/
+cd $BUILD_DIR/bin/Debug
 
-XMLFILE=$BUILD_DIR/bin/valgrind_TestDebruijn_${BUILD_NUMBER}.xml
-cd $BUILD_DIR/bin
+./kmer1
 
-#------------ TestDebruijn without valgrind --------------------
-./gatb-core-cppunit TestDebruijn || error_code
-
-
-#------------ TestDebruijn with valgrind -----------------------
-
-valgrind --xml=yes --xml-file=$XMLFILE \
-	./gatb-core-cppunit TestDebruijn || error_code
-
-xmllint --xpath "string(//error)" $XMLFILE
-
-xmllint --xpath "string(//errorcounts)" $XMLFILE
-
-#>>>>>>>>>>>> 2. Diagnostics for dbgh5 >>>>>>>>>>>>>>>>>>>>>>>>>
-
-XMLFILE=$BUILD_DIR/bin/valgrind_dbgh5_${BUILD_NUMBER}.xml
-valgrind --xml=yes --xml-file=$XMLFILE \
-	$BUILD_DIR/bin/dbgh5 -verbose 0 -in $HOME/reference/fastq/aphid_662451seq.fa -check $HOME/reference/check/aphid_662451.props || error_code
-
-xmllint --xpath "string(//error)" $XMLFILE
-
-xmllint --xpath "string(//errorcounts)" $XMLFILE
 
 
 ################################################################
