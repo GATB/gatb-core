475804999313e75008730db8330fe51a3a50c9b1
Patrick Durand <patrick.durand@inria.fr>
2016-10-25 11:52:42 +0200

    remove required threshold argument

--- gatb-core/examples/bank/bank7.cpp
+++ gatb-core/examples/bank/bank7.cpp
@@ -1,62 +1,64 @@
 //! [snippet1]
-// We include what we need for the test
+
+// We include GATB-Core
 #include <gatb/gatb_core.hpp>
 #include <iostream>
 
 // We use the required packages
 using namespace std;
 
+// sequence size threshold; see its use in the FilterFunctor
 size_t threshold = 500;
 
-// We a define a functor that will be called during iteration for filtering odd items.
-// Here, we keep sequences whose data size is greater than 500.
-struct FilterFunctor  {  bool operator ()  (Sequence& seq) const  {  return seq.getDataSize() >= threshold; } };
+// We a define a functor that will be called during iteration for 
+// filtering sequences.
+struct FilterFunctor  {  
+  bool operator ()  (Sequence& seq) const  {  
+    // Here, we keep sequences whose size is greater than 500.
+    return seq.getDataSize() >= threshold; 
+  }
+};
 
 /********************************************************************************/
 /*                                Bank filtering                                */
 /*                                                                              */
 /* This snippet shows how to iterate a bank and filter some sequences through   */
 /* a functor.                                                                   */
-/* Note: lambda expressions could be used for the functor (in case the used     */
-/* compiler supports it)                                                        */
 /*                                                                              */
 /********************************************************************************/
 int main (int argc, char* argv[])
 {
-    if (argc < 3)
+  // We check that the user provides at least one option: a Fasta/FastQ file.
+  // Online GATB-Tutorial: this argument is automatically filled in with an 
+  // appropriate file.
+  if (argc < 2)
+  {
+    std::cerr << "Please, provide a sequence file." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  try
+  {
+    // We declare an input Bank and use it locally
+    IBank* inputBank = Bank::open (argv[1]);
+    LOCAL (inputBank);
+
+    // We use another iterator for filtering out some sequences.
+    FilterIterator<Sequence,FilterFunctor> itSeq (inputBank->iterator(), FilterFunctor());
+
+    // We loop over sequences.
+    for (itSeq.first(); !itSeq.isDone(); itSeq.next())
     {
-        cerr << "you must provide at least the size threshold and the bank file path. Arguments are:" << endl;
-        cerr << "   1) bank" << endl;
-        cerr << "   2) minimum size threshold (default 500)" << endl;
-        return EXIT_FAILURE;
+      // We dump the sequence size and its comment
+      cout << "[" << itSeq->getDataSize() << "] " << itSeq->getComment()  << endl;
+
+      // We could also dump the sequence
+      //cout << itSeq->toString() << endl;
     }
-
-    // We define a try/catch block in case some method fails (bad filename for instance)
-    try
-    {
-        // We declare an input Bank and use it locally
-        IBank* inputBank = Bank::open (argv[1]);
-        LOCAL (inputBank);
-
-        // We set the threshold with given parameter
-        threshold = atoi (argv[2]);
-
-        // We use another iterator for filtering out some sequences.
-        FilterIterator<Sequence,FilterFunctor> itSeq (inputBank->iterator(), FilterFunctor());
-        
-        // We loop over sequences.
-        for (itSeq.first(); !itSeq.isDone(); itSeq.next())
-        {
-            // We dump the data size and the comment
-            cout << "[" << itSeq->getDataSize() << "] " << itSeq->getComment()  << endl;
-
-            // We dump the data
-            cout << itSeq->toString() << endl;
-        }
-    }
-    catch (Exception& e)
-    {
-        cerr << "EXCEPTION: " << e.getMessage() << endl;
-    }
+  }
+  catch (Exception& e)
+  {
+    cerr << "EXCEPTION: " << e.getMessage() << endl;
+  }
 }
 //! [snippet1]
