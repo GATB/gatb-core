55437812df961d3ec8a9d979c98590476a68be27
Patrick Durand <patrick.durand@inria.fr>
2016-10-27 14:32:09 +0200

    add OptionParser snippets for online tutorial

--- gatb-core/examples/tools/optionsparser1.cpp
+++ gatb-core/examples/tools/optionsparser1.cpp
@@ -1,45 +1,78 @@
 //! [snippet1]
 
-// We include what we need for the test
+/********************************************************************************/
+/*                                                                              */
+/*      Command line parsing using OptionsParser and inheritance                */
+/*      of arguments coming from other GATB-Core components.                    */          
+/*                                                                              */
+/********************************************************************************/
+
+// We include GATB-Core
 #include <gatb/gatb_core.hpp>
+
 #include <list>
 #include <iostream>
 
 /********************************************************************************/
-/*                Command line parsing through IOptionParser                    */
-/********************************************************************************/
+// START Application
 int main (int argc, char* argv[])
 {
-    IOptionsParser* root = new OptionsParser ("root");
+  // We create a new parser ang give it a name
+  IOptionsParser* root = new OptionsParser ("root");
 
-    root->push_back (SortingCountAlgorithm<>::getOptionsParser(false));
-    root->push_back (DebloomAlgorithm<>::getOptionsParser());
+  // inherit arguments from SortingCountAlgorithm and we request that none of
+  // its argument are mandatory
+  root->push_back (SortingCountAlgorithm<>::getOptionsParser(false));
 
-    IOptionsParser* child1 = new OptionsParser ("child1");
+  // Now, we setup our own parser
+  IOptionsParser* child1 = new OptionsParser ("child1");
+  
+  // and we add that specific parser to the main parser that also
+  // contains arguments from Graph
+  root->push_back (child1);
 
-    root->push_back (child1);
+  // we declare are own arguments
+  child1->push_back (
+    new OptionOneParam (
+      STR_NB_CORES,
+      "nb cores (0 for all)",
+      false,
+      "0"
+    ));
+  child1->push_back (
+    new OptionOneParam (
+      STR_MAX_MEMORY, 
+      "max memory (in MBytes)", 
+      false, 
+      "2000"
+    ));
+  child1->push_back (
+    new OptionOneParam (
+      STR_MAX_DISK, 
+      "max disk (in MBytes)",
+      false,
+      "0"
+    ));
 
-    if (IOptionsParser* bloomParser = root->getParser ("bloom"))  {  bloomParser->setVisible(false);  }
+  try
+  {
+    root->parse (argc, argv);
+  }
+  catch (OptionFailure& e)
+  {
+    e.displayErrors(std::cout);
+  }
 
-    child1->push_back (new OptionOneParam (STR_NB_CORES,          "nb cores (0 for all)",   false, "0"));
-    child1->push_back (new OptionOneParam (STR_MAX_MEMORY,        "max memory (in MBytes)", false, "2000"));
-    child1->push_back (new OptionOneParam (STR_MAX_DISK,          "max disk   (in MBytes)", false, "0"));
+  // We can dump the many arguments and their values
+  printf ("-------------------------------------------------------------\n");
+  RawDumpPropertiesVisitor v;
+  root->getProperties()->accept (&v);
 
-    child1->push_back (BranchingAlgorithm<>::getOptionsParser());
+  // So, from this point we can use argument's values in the software.
 
-    try
-    {
-        root->parse (argc, argv);
-    }
-    catch (OptionFailure& e)
-    {
-        e.displayErrors(std::cout);
-    }
+  // .../...
 
-    printf ("-------------------------------------------------------------\n");
-    RawDumpPropertiesVisitor v;
-    root->getProperties()->accept (&v);
-
-    delete root;
+  // when we do not need anymore the parser... delete it!
+  delete root;
 }
 //! [snippet1]
--- gatb-core/examples/tools/optionsparser2.cpp
+++ gatb-core/examples/tools/optionsparser2.cpp
@@ -1,28 +1,67 @@
 //! [snippet1]
+// GATB-Core Online Tutorial 
 
-// We include what we need for the test
+/********************************************************************************/
+/*                                                                              */
+/*                Command line parsing through OptionsParser                    */
+/*                                                                              */
+/********************************************************************************/
+
+// We include GATB-Core
 #include <gatb/gatb_core.hpp>
 #include <iostream>
 
 /********************************************************************************/
-/*                Command line parsing through IOptionParser                    */
-/********************************************************************************/
+// START Application
 int main (int argc, char* argv[])
 {
-    IOptionsParser* root = new OptionsParser ("root");
-    LOCAL (root);
+  // We create a new parser and give it a name
+  OptionsParser parser ("CmdLineSnippet");
 
-    root->push_back (new OptionNoParam (STR_HELP, "help",   false));
+  // first argument of our program
+  parser.push_back (
+    // this is the class to use to setup an argument that requires a value
+    new OptionOneParam (
+      "-g",          // argument to use on the cmd-line
+      "graph input", // help message
+      true           // is it mandatory?
+    ));
 
-    try
-    {
-        root->parse (argc, argv);
-    }
-    catch (OptionFailure& e)
-    {
-        e.displayErrors(std::cout);
-    }
+  //second argument
+  parser.push_back (
+    new OptionOneParam (
+      "-v",   
+      "verbosity (0:no display, 1: display kmers, 2: display distrib",  
+      false, 
+      "0" // default value
+    ));
 
-    std::cout << "status=" << root->saw(STR_HELP) << std::endl;
+  //third argument; no value expected
+  parser.push_back (
+    // this is the class to use to setup an argument that 
+    // DOES NOT require a value
+    new OptionNoParam (
+      "-h", "help", false
+    ));
+
+  // Now let's start the business of our application
+  try
+  {
+    // First to do: analyse the software arguments using our Parser.
+    // Here, if a mandatory argument is missing (-g in this example), then
+    // an exception is raised and the software automatically dump a full help
+    // message on the command-line... thanks to GATB-Core OptionsParser API.
+    IProperties* options = parser.parse (argc, argv);
+
+    // Here is how to retrieve the value for a particular option
+    std::cout << "graph input file is: =" << options->getStr("-g") << std::endl;
+
+    // Here is how to check if we have a particular no-value argument
+    std::cout << "help requested? " << parser.saw("-h") << std::endl;
+  }
+  catch (OptionFailure& e)
+  {
+    e.displayErrors(std::cout);
+  }
 }
 //! [snippet1]
